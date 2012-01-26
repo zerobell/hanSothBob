e7aba3da21acbee141d4b9d0afe7ebc16400bb02
Jonghoon Kim <zerobell@ovi.com>
2012-01-26 11:13:15 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var port = 4081;
+var host = "hansothbob.zerobell.cloud9ide.com";
 var server = require('./basicserver').createServer();
 
-server.useFavIcon("172.16.120.30", "./docroot/favicon.png");
+server.useFavIcon(host, "./docroot/favicon.png");
 server.addContainer(".*", "/l/(.*)$", require('./redirector'), { });
 
 //server.docroot("172.16.120.30", "/", "./docroot");
-server.docroot("172.16.120.30", "/", "./jqtouch");
+server.docroot(host, "/", "./jqtouch");
 
 
 require('./httpsniffer').sniffOn(server);
