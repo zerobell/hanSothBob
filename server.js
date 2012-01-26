7f7f9c65c9311d893d1c4ae8ca0ca26ae1296f57
Jonghoon <zerobell@ovi.com>
2012-01-26 13:20:18 +0900

    hanSothBob

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var port = 4081;
 var server = require('./basicserver').createServer();
 
 server.useFavIcon("172.16.120.30", "./docroot/favicon.png");
+server.addContainer(".*", "/show(.*)$", require('./show'), { });
 server.addContainer(".*", "/l/(.*)$", require('./redirector'), { });
 
 //server.docroot("172.16.120.30", "/", "./docroot");
-server.docroot("172.16.120.30", "/", "./jqtouch");
+//server.docroot("172.16.120.30", "/", "./jqtouch");
+server.docroot("172.16.120.30", "/", "./html5");
 
 
 require('./httpsniffer').sniffOn(server);
